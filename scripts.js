546f99c7d6c35a543ade90095432e1ce1c82eaaa
Yusif Bashirov <yusifbesir1@gmail.com>
2024-12-01 21:44:24 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,67 @@
+/*
+    Code sample for SITE 1101 Principles of Information Systems 
+    (c)2024 by Araz Yusubov 
+    DISCLAIMER: All code examples we will look at are quick hacks intended to present working prototypes.
+    Hence they do not follow best practice of programming or software engineering.    
+*/
+
+// Global variables for Artist's position and orientation
+var x, y;
+var angle;
+
+function radian(degree) {
+    return degree * Math.PI / 180;
+}
+
+function moveForward(distance, context) {
+    let a = radian(angle);
+    x = x + distance * Math.cos(a);
+    y = y + distance * Math.sin(a);
+    context.lineTo(x, y);
+}
+
+function turnRight(degree) {
+    angle = angle - degree;
+    if (angle < 0) angle = angle + 360;
+}
+
+function turnLeft(degree) {
+    angle = angle + degree;
+    if (angle > 360) angle = angle - 360;
+}
+
+function DrawSpiral(context) {
+    // Inspired by Express Course (2024) Lesson 29: For Loops with Artist
+    // https://studio.code.org/s/express-2024/lessons/29/levels/5
+
+    // The initial position is in the center of the canvas
+    x = context.canvas.width / 2;
+    y = context.canvas.height / 2;
+    // The initial orientation is zero degrees i.e. facing East
+    angle = 0.0;
+    context.moveTo(x, y);
+    context.beginPath();
+    for (let counter = 3; counter < 600; counter += 3) {
+        moveForward(counter, context);
+        context.stroke();
+        turnRight(89);
+    }
+}
+function DrawStar(context, size) {
+    for (let i = 0; i < 5; i++) {
+        moveForward(size, context); 
+        turnRight(144);
+    }
+}
+function DrawStarPattern(context) {
+    x = context.canvas.width / 2;
+    y = context.canvas.height / 2;
+    angle = 0.0;
+    context.beginPath();
+    for (let i = 1; i <= 10; i++) {
+        context.strokeStyle = `rgb(${255 * Math.random()}, ${255 * Math.random()}, ${255 * Math.random()})`;
+        DrawStar(context, i * 20);
+        turnRight(30);
+        context.stroke();
+    }
+}
